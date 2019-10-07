0dcd156ba8604ae40c1d3e2396d2fbeea478018c
MATRAX21 <51673733+MATRAX21@users.noreply.github.com>
2019-10-07 21:21:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const devs = ['459472898655256586'];
 const db = require('quick.db');
-const premium = ['470896018603376640']
+const premium = ['459472898655256586']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
